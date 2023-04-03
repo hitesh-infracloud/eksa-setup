66e5924702995f19e4502753f9249060c0ae1e06
hitesh-infracloud <hitesh.pattanayak@infracloud.io>
2023-04-03 20:01:37 +0530

    eksa-setup | install yq tool in admin machine

--- scripts/configure-admin.sh
+++ scripts/configure-admin.sh
@@ -11,13 +11,16 @@ gpasswd -a vagrant docker
 # install kubectl cli tool
 curl -LO https://dl.k8s.io/v1.25.2/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-mv ./kubectl /usr/local/bin/kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
 
 # install latest eksctl cli tool and eksctl-anywhere plugin
 curl "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" \
     --silent --location \
     | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin/
+
+# install yq tool
+snap install yq
 
 RELEASE_VERSION=$(curl https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml --silent --location | yq ".spec.latestVersion")
 EKS_ANYWHERE_TARBALL_URL=$(curl https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml --silent --location | yq ".spec.releases[] | select(.version==\"$RELEASE_VERSION\").eksABinary.$(uname -s | tr A-Z a-z).uri")
