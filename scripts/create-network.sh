256bfd3c9ad1ea438f60e342db2751b8d4a939e2
hitesh-infracloud <hitesh.pattanayak@infracloud.io>
2023-08-01 17:02:35 +0530

    eksa-setup | flush iptables and populate network config file

--- scripts/create-network.sh
+++ scripts/create-network.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-# Create NAT network
-VBoxManage natnetwork add --netname eksa-net --network "192.168.10.0/24" --dhcp off
+VM_DIR='/root/eksa/vms'
+NETWORK_CONFIG_FILE="${VM_DIR}/network_config"
 
-# Configure NAT network
-VBoxManage natnetwork modify --netname eksa-net --ipv6 off --port-forward-4 "ssh:tcp:[]:2222:[192.168.10.4]:22"
+
+exit_out() {
+    echo ${1} && echo && exit -1
+}
+
+flush-iptables() {
+    echo -e "[+] Flushing local iptables"
+    iptables -F; iptables -t nat -F; netfilter-persistent save  > /dev/null 2>&1
+}
+
+EKSA_NET='192.168.10.0/24'
+GATEWAY='192.168.10.1'
+
+
+echo -e "[+] Creating natnetwork eksa-net with cidr ${EKSA_NET} and gateway ${GATEWAY}"
+VBoxManage natnetwork add --netname eksa-net --network "192.168.10.0/24" --dhcp off
+[ $? -ne 0 ] && exit_out "Detected an error while creating natnetwork eksa-net...exiting!!"
+
+VBoxManage natnetwork start --netname eksa-net
+
+echo -e "[+] Populating network config ${NETWORK_CONFIG_FILE}"
+echo -e "NetworkCidr:$EKSA_NET\nNetworkGateway:${GATEWAY}" > ${NETWORK_CONFIG_FILE}
+cat ${NETWORK_CONFIG_FILE}
+
+exit 0
